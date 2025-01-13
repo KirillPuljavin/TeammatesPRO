ac945d057b1877abaeac1279d9b9383b2b12af4d
SFG <kirill.puljavin@gmail.com>
2025-01-13 11:08:59 +0100

    config fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
 
+  distDir: ".next",
+
   env: {
     TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
     TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
